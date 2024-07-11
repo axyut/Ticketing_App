b34dac3725978ecfe63b7870ab74843170c04ce2
axyut <aoogleak@gmail.com>
2024-07-11 11:24:35 +0545

    feat:client_completion

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,23 +1,41 @@
-import axios from "axios";
+import Link from 'next/link';
 
-const LandingPage = ({ currentUser }) => {
-    // console.log(currentUser);
-    axios.get("http://localhost:3000/api/users/currentuser");
-    return currentUser ? (
-        <h1>You are signed in</h1>
-    ) : (
-        <h1>You are NOT signed in</h1>
+const LandingPage = ({ currentUser, tickets }) => {
+  const ticketList = tickets.map((ticket) => {
+    return (
+      <tr key={ticket.id}>
+        <td>{ticket.title}</td>
+        <td>{ticket.price}</td>
+        <td>
+          <Link href="/tickets/[ticketId]" as={`/tickets/${ticket.id}`}>
+            <a>View</a>
+          </Link>
+        </td>
+      </tr>
     );
+  });
+
+  return (
+    <div>
+      <h1>Tickets</h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Price</th>
+            <th>Link</th>
+          </tr>
+        </thead>
+        <tbody>{ticketList}</tbody>
+      </table>
+    </div>
+  );
 };
 
-// LandingPage.getInitialProps = async () => {
-//   console.log("LANDING PAGE!");
-//   // request being sent has no cookies attached to it because it is being sent from the server
-//   const { data } = await axios.get(
-//     "http://localhost:3000/api/users/currentuser"
-//   );
-//   console.log(data);
-//   return data;
-// };
+LandingPage.getInitialProps = async (context, client, currentUser) => {
+  const { data } = await client.get('/api/tickets');
+
+  return { tickets: data };
+};
 
 export default LandingPage;
